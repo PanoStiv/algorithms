3a5c312688d63e0bd57c44073b97f3b84bcda496
PanoStiv <pan.stivaktas@gmail.com>
2021-10-07 19:09:08 +0300

    Add files via upload

---
+++ factorial/iterative function/factorial_iterative.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    p = 1
+    for i in range(2, n+1):
+        p = p * i
+
+    return p
+
+
+#test
+for i in range(2, 50):
+    print(factorial(i))
---
+++ factorial/recursive function/factorial_recursive.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+#test
+for i in range(1,11):
+    print(f"factorial({i})={factorial(i)} ")
