26656d7525d2dc99a5fcdbc63bf215642b9d7ce2
PanoStiv <pan.stivaktas@gmail.com>
2021-10-07 23:56:55 +0300

    Add files via upload

---
+++ Bubble Sort/bubble_sort.py
@@ -0,0 +1,6 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(n-1,i,-1):
+            if array[j] < array[j-1]:
+                array[j], array[j-1] = array[j-1], array[j]
